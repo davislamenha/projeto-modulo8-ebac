1686ec7df4bbe34e1eff395742aa7279779baf6f
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-02-13 08:56:24 -0300

    organização

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 const form = document.getElementById('form-atividade');
 
+let linhas = '';
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
+  adicionarLinha();
+  atualizaTabela();
+});
+
+function adicionarLinha() {
   const inputNomeAtividade = document.getElementById('nome-atividade');
   const inputNotaAtividade = document.getElementById('nota-atividade');
+
+  atividades.push(inputNomeAtividade.value);
+  notas.push(parseFloat(inputNotaAtividade.value));
+
   const imgEmoji =
     +inputNotaAtividade.value >= 7
       ? '<img src="./img/aprovado.png" alt="Emoji festejando">'
@@ -17,8 +28,13 @@ form.addEventListener('submit', (e) => {
       <td>${imgEmoji}</td>
   </tr>`;
 
-  const corpoTabela = document.querySelector('tbody');
-  corpoTabela.innerHTML += linha;
+  linhas += linha;
+
   inputNomeAtividade.value = '';
   inputNotaAtividade.value = '';
-});
+}
+
+function atualizaTabela() {
+  const corpoTabela = document.querySelector('tbody');
+  corpoTabela.innerHTML = linhas;
+}
